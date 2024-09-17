25053ef9ce7d10e41637ca22802ef75b359e57e0
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-17 05:45:08 +0530

    Create page.tsx

---
+++ app/wishlist/page.tsx
@@ -0,0 +1,9 @@
+export default function WishlistPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-8">Your Wishlist</h1>
+      <p>Your wishlist is empty.</p>
+      {/* Add wishlist functionality here */}
+    </div>
+  )
+}
