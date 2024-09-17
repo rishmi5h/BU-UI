3236d5faafd87390354090c11c1f6afb03c86c97
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-17 05:44:48 +0530

    Create Footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 py-4 mt-8">
+      <div className="container mx-auto px-4 text-center">
+        <p>&copy; 2023 Black Unlimited. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
