10cb3860e5373ecfed231bc6a0065cc8f7c03ef4
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-17 05:44:44 +0530

    Create page.tsx

---
+++ app/cart/page.tsx
@@ -0,0 +1,9 @@
+export default function CartPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-8">Your Cart</h1>
+      <p>Your cart is empty.</p>
+      {/* Add cart functionality here */}
+    </div>
+  )
+}
