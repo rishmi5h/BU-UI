b07456272757d1ca01c377375569cebcbb6a8e7a
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-17 06:11:11 +0530

    Create WishlistContext.tsx

---
+++ app/context/WishlistContext.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+type WishlistItem = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+type WishlistContextType = {
+  wishlist: WishlistItem[];
+  addToWishlist: (item: WishlistItem) => void;
+  removeFromWishlist: (id: number) => void;
+  isInWishlist: (id: number) => boolean;
+};
+
+const WishlistContext = createContext<WishlistContextType | undefined>(undefined);
+
+export function WishlistProvider({ children }: { children: React.ReactNode }) {
+  const [wishlist, setWishlist] = useState<WishlistItem[]>([]);
+
+  useEffect(() => {
+    const savedWishlist = localStorage.getItem('wishlist');
+    if (savedWishlist) {
+      setWishlist(JSON.parse(savedWishlist));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+  }, [wishlist]);
+
+  const addToWishlist = (item: WishlistItem) => {
+    setWishlist((prev) => {
+      if (!prev.some((wishlistItem) => wishlistItem.id === item.id)) {
+        return [...prev, item];
+      }
+      return prev;
+    });
+  };
+
+  const removeFromWishlist = (id: number) => {
+    setWishlist((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const isInWishlist = (id: number) => {
+    return wishlist.some((item) => item.id === id);
+  };
+
+  return (
+    <WishlistContext.Provider
+      value={{ wishlist, addToWishlist, removeFromWishlist, isInWishlist }}
+    >
+      {children}
+    </WishlistContext.Provider>
+  );
+}
+
+export function useWishlist() {
+  const context = useContext(WishlistContext);
+  if (context === undefined) {
+    throw new Error('useWishlist must be used within a WishlistProvider');
+  }
+  return context;
+}
