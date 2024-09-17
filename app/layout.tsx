13a26ef8d85adeb3c48ace79eb7aa95879c47549
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-17 06:04:52 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,40 @@
-import './globals.css'
-import { Playfair_Display, Lato } from 'next/font/google'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import "./globals.css";
+import { Playfair_Display, Lato } from "next/font/google";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { CartProvider } from "./context/CartContext";
 
-const playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfair' })
-const lato = Lato({ subsets: ['latin'], weight: ['300', '400', '700'], variable: '--font-lato' })
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair",
+});
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--font-lato",
+});
 
 export const metadata = {
-  title: 'Black Unlimited - Minimalist Fashion',
-  description: 'Discover our collection of elegant black clothing',
-}
+  title: "Black Unlimited - Minimalist Fashion",
+  description: "Discover our collection of elegant black clothing",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${playfair.variable} ${lato.variable}`}>
-      <body className="bg-black text-white min-h-screen flex flex-col font-sans">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-12">
-          {children}
-        </main>
-        <Footer />
+    <html lang='en' className={`${playfair.variable} ${lato.variable}`}>
+      <body className='bg-black text-white min-h-screen flex flex-col font-sans'>
+        <CartProvider>
+          <Header />
+          <main className='flex-grow container mx-auto px-4 py-12'>
+            {children}
+          </main>
+          <Footer />
+        </CartProvider>
       </body>
     </html>
-  )
+  );
 }
