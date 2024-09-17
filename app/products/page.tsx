c6a9947eccd53aca67629dd336b40eed982112f9
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-17 06:04:55 +0530

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,17 +3,19 @@
 import Link from "next/link";
 import { FaHeart, FaShoppingBag } from "react-icons/fa";
 import { useState } from "react";
+import { useCart } from "../context/CartContext";
 
 export default function ProductsPage() {
   const [wishlist, setWishlist] = useState<number[]>([]);
+  const { addToCart } = useCart();
 
   const products = [
-    { id: 1, name: "Black T-Shirt", price: 29.99 },
-    { id: 2, name: "Black Jeans", price: 59.99 },
-    { id: 3, name: "Black Dress", price: 79.99 },
-    { id: 4, name: "Black Sweater", price: 49.99 },
-    { id: 5, name: "Black Jacket", price: 89.99 },
-    { id: 6, name: "Black Skirt", price: 39.99 },
+    { id: 1, name: "Black T-Shirt", price: 1999 },
+    { id: 2, name: "Black Jeans", price: 3999 },
+    { id: 3, name: "Black Dress", price: 5999 },
+    { id: 4, name: "Black Sweater", price: 3499 },
+    { id: 5, name: "Black Jacket", price: 6999 },
+    { id: 6, name: "Black Skirt", price: 2999 },
   ];
 
   const toggleWishlist = (id: number) => {
@@ -41,12 +43,17 @@ export default function ProductsPage() {
             </button>
             <Link href={`/products/${product.id}`}>
               <h2 className='text-xl font-semibold mb-2'>{product.name}</h2>
-              <p className='text-gray-400 mb-4'>${product.price.toFixed(2)}</p>
-              <button className='flex items-center justify-center w-full bg-white text-black px-4 py-2 rounded-full hover:bg-gray-200 transition-colors text-sm uppercase tracking-wide font-bold'>
-                <FaShoppingBag className='mr-2' />
-                Add to Bag
-              </button>
+              <p className='text-gray-400 mb-4'>
+                ₹{product.price.toLocaleString("en-IN")}
+              </p>
             </Link>
+            <button
+              onClick={() => addToCart({ ...product, quantity: 1 })}
+              className='flex items-center justify-center w-full bg-white text-black px-4 py-2 rounded-full hover:bg-gray-200 transition-colors text-sm uppercase tracking-wide font-bold'
+            >
+              <FaShoppingBag className='mr-2' />
+              Add to Bag
+            </button>
           </div>
         ))}
       </div>
