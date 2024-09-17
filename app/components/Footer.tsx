0146ae2e4e840d929b74a843c547eec5879eb0b1
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-17 05:50:49 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,44 @@
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaPinterest,
+} from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <footer className="bg-black py-8 border-t border-gray-800">
-      <div className="container mx-auto px-4 text-center">
-        <p className="text-sm text-gray-400">&copy; 2024 Black Unlimited. All rights reserved.</p>
+    <footer className='bg-black py-8 border-t border-gray-800'>
+      <div className='container mx-auto px-4 text-center'>
+        <div className='flex justify-center space-x-6 mb-4'>
+          <a
+            href='#'
+            className='text-gray-400 hover:text-white transition-colors'
+          >
+            <FaFacebookF />
+          </a>
+          <a
+            href='#'
+            className='text-gray-400 hover:text-white transition-colors'
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href='#'
+            className='text-gray-400 hover:text-white transition-colors'
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href='#'
+            className='text-gray-400 hover:text-white transition-colors'
+          >
+            <FaPinterest />
+          </a>
+        </div>
+        <p className='text-sm text-gray-400'>
+          &copy; 2024 Black Unlimited. All rights reserved.
+        </p>
       </div>
     </footer>
-  )
+  );
 }
