10328438a6a2850761fce1893c71365485e4a97d
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-17 19:55:53 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,36 +7,36 @@ import {
 
 export default function Footer() {
   return (
-    <footer className='bg-black py-8 border-t border-gray-800'>
+    <footer className='bg-black py-6 sm:py-8 border-t border-gray-800'>
       <div className='container mx-auto px-4 text-center'>
-        <div className='flex justify-center space-x-6 mb-4'>
+        <div className='flex justify-center space-x-4 sm:space-x-6 mb-4'>
           <a
             href='#'
             className='text-gray-400 hover:text-white transition-colors'
           >
-            <FaFacebookF />
+            <FaFacebookF className="text-xl sm:text-2xl" />
           </a>
           <a
             href='#'
             className='text-gray-400 hover:text-white transition-colors'
           >
-            <FaTwitter />
+            <FaTwitter className="text-xl sm:text-2xl" />
           </a>
           <a
             href='https://www.instagram.com/black.unlimited/'
             className='text-gray-400 hover:text-white transition-colors'
             target='_blank'
           >
-            <FaInstagram />
+            <FaInstagram className="text-xl sm:text-2xl" />
           </a>
           <a
             href='#'
             className='text-gray-400 hover:text-white transition-colors'
           >
-            <FaPinterest />
+            <FaPinterest className="text-xl sm:text-2xl" />
           </a>
         </div>
-        <p className='text-sm text-gray-400'>
+        <p className='text-sm sm:text-base text-gray-400'>
           &copy; 2024 Black Unlimited. All rights reserved.
         </p>
       </div>
