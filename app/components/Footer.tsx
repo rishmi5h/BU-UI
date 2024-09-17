cec396679cbd577626d57225ab7a4c77146f9438
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-17 05:46:37 +0530

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900 py-4 mt-8">
       <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2023 Black Unlimited. All rights reserved.</p>
+        <p>&copy; 2024 Black Unlimited. All rights reserved.</p>
       </div>
     </footer>
   )
